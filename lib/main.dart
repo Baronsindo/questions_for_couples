be313dc3cbbb958e499957eeb26a19a17d833ab6
Adil <baronsindo@gmail.com>
2021-10-07 11:49:08 +0100

    zedt ads o gadithom

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:questions_for_couples/tools/app_constant.dart';
 import 'package:splash_screen_view/SplashScreenView.dart';
 import 'package:questions_for_couples/screens/HomePage.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  Future<InitializationStatus> _initGoogleMobileAds() {
-    // TODO: Initialize Google Mobile Ads SDK
-    return MobileAds.instance.initialize();
-  }
+  // Future<InitializationStatus> _initGoogleMobileAds() {
+  //   // TODO: Initialize Google Mobile Ads SDK
+  //   return MobileAds.instance.initialize();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    _initGoogleMobileAds();
+    // _initGoogleMobileAds();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
